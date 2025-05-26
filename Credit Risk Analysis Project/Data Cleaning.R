aad83c17ee84c61752d4ff8d1b3a06c020605a28
Savero Fernando Tandra <162685507+SaveroFT@users.noreply.github.com>
2025-05-26 16:49:33 +0800

    Add files via upload

---
+++ Credit Risk Analysis Project/Data Cleaning.R
@@ -0,0 +1,20 @@
+# Rules for data cleaning
+# 1st rule: Maximum rows you can remove: 600. Don't remove any rows is the best.
+# Replace any voices with other values
+# State the final number of rows of dataset in documentation
+
+# The best is to clean whole dataset, but you can choose to clean
+# The columns you selected in your hypothesis or clean few more extra variables
+
+# For documentation of data cleaning, just write the columns selected for hypothesis
+
+# Data Cleaning is a group work, compile the data cleaning codes within 1 R script
+# then export it as csv to let other members to import dataset for data analysis
+
+# Techniques for doing imputation (replace NA)
+# Mean/median/mode imputation (only this covered in PFDA)
+# KNN imputation
+# Linear/logistic regression imputation
+# Random forest imputation
+# Hot deck imputation
+# Cold deck imputation
